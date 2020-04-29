4eac8ced7838d5b98ea5cccce4ac64f524c3eee7
ethanumillard <ethanumillard@gmail.com>
2020-04-29 15:13:31 -0600

    Updates

---
+++ precache-manifest.68bb9432714211b7ed913e950de207f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d560416e7a34719d974b1df17de15d4",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "5379e2e34e133a2c8a6f",
+    "url": "/memory/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0565843f635093bd75e6",
+    "url": "/memory/static/css/main.9b42d220.chunk.css"
+  },
+  {
+    "revision": "5379e2e34e133a2c8a6f",
+    "url": "/memory/static/js/2.8a76382a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/memory/static/js/2.8a76382a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0565843f635093bd75e6",
+    "url": "/memory/static/js/main.078baac9.chunk.js"
+  },
+  {
+    "revision": "0548a1405ac133eb3063",
+    "url": "/memory/static/js/runtime-main.0816c3e2.js"
+  },
+  {
+    "revision": "82b2042d1f9e1191498843f2cb476a16",
+    "url": "/memory/static/media/card.82b2042d.jpg"
+  }
+]);
